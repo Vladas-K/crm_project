e21dd8b872f41ddfe262207ba4f295801207b610
Vladas <vlad250577@yandex.ru>
2026-05-29 22:24:13 +0300

    Удаление лишнего импорта static_version из шаблонов для упрощения кода

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -43,6 +43,9 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "builtins": [
+                "core.templatetags.static_version",
+            ],
         },
     },
 ]
