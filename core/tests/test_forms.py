5a1179cc86b0657e44858e4d2fb28f5b1e62987d
Vladas <vlad250577@yandex.ru>
2026-09-13 21:34:49 +0300

    Убрать текстовое поле подрядчика из формы расходов

--- core/tests/test_forms.py
+++ core/tests/test_forms.py
@@ -1,4 +1,4 @@
-from core.forms import EventForm
+from core.forms import EventExpenseForm, EventForm
 
 
 def test_event_form_preserves_event_date_in_date_input(crm_objects):
@@ -9,3 +9,12 @@ def test_event_form_preserves_event_date_in_date_input(crm_objects):
 
     assert f'value="{event.date:%Y-%m-%d}"' in rendered_date
     assert f'value="{event.date:%d.%m.%Y}"' not in rendered_date
+
+
+def test_expense_form_uses_linked_vendor_assignment(crm_objects):
+    """Форма расхода использует назначенного подрядчика вместо свободного текста."""
+
+    form = EventExpenseForm()
+
+    assert "vendor_assignment" in form.fields
+    assert "vendor_name" not in form.fields
